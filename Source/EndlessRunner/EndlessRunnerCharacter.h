bf4dc13949a5fdfe0ba587daa401bc517d304621
TalimeQ <boren1996@wp.pl>
2019-04-04 15:21:02 +0200

    #Base Game Setup, Character Controls, Base Tile, Tile spawning

---
+++ Source/EndlessRunner/EndlessRunnerCharacter.h
@@ -0,0 +1,63 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EndlessRunnerCharacter.generated.h"
+
+UCLASS(config=Game)
+class AEndlessRunnerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+public:
+	AEndlessRunnerCharacter();
+
+
+protected:
+
+	virtual void Tick(float deltatime) override;
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+	void TurnNinety(float value);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Movement", BlueprintReadWrite)
+		float movementSpeed = 1.0f;
+
+	UPROPERTY(Category = "Movement", BlueprintReadWrite)
+		bool bCanTurn = false;
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
+
+public:
+
+	/* Sets if player can turn */
+	UFUNCTION(BlueprintCallable,Category = "Movement")
+	void SetCanTurn(bool bNewValue);
+	/* Returns turn value */
+	UFUNCTION(BlueprintPure, Category = "Movement")
+	bool GetCanTurn();
+
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+private:
+	FRotator desiredRot{ 0,0,0};
+};
+
